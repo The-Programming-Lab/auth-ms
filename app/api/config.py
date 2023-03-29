348ff87c6c35ac7f6433bfb7a5d0d9280f10b65e
BraedenHCL <braeden.norman6@gmail.com>
2023-03-29 01:04:40 -0700

    testing container env variables.

--- app/api/config.py
+++ app/api/config.py
@@ -3,9 +3,19 @@ from dotenv import load_dotenv
 
 
 if "APP_ENV" in os.environ and os.environ['APP_ENV'] == 'production':
-    hello_world = os.environ['HELLO_WORLD']
-    health_check_endpoint = os.environ['HEALTH_CHECK_ENDPOINT']
-    base_path = os.environ['BASE_PATH']
+    try:
+        hello_world = os.environ['HELLO_WORLD']
+    except:
+        hello_world = 'An error occurred while trying to get the environment variable HELLO_WORLD'
+    try:
+        health_check_endpoint = os.environ['HEALTH_CHECK_ENDPOINT']
+    except:
+        health_check_endpoint = '/health-check'
+    try:
+        base_path = os.environ['BASE_PATH']
+    except:
+        base_path = '/error'
+    
 
 else:
     load_dotenv('.env')
