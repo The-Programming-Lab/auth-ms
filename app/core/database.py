624a7edc869e9aaae1ec64ea340d71ee0a5cebce
BraedenHCL <braeden.norman6@gmail.com>
2023-06-26 20:28:51 -0700

    added google keys and logging

---
+++ app/core/database.py
@@ -0,0 +1,13 @@
+from firebase_admin import credentials, initialize_app, firestore
+import os
+
+from app.core.logging import logger
+
+# initialize firebase auth and db
+try :
+    cred = credentials.Certificate(os.getenv("GOOGLE_KEY_PATH"))
+    initialize_app(cred)
+    db = firestore.client()
+except Exception as e:
+    logger.exception(e)
+    db = None
